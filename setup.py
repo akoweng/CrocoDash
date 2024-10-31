2e990b89c6eeba74ff804f54a2cda6efda2c4a5a
alperaltuntas <alperaltuntas@gmail.com>
2024-10-31 14:25:31 -0600

    install self (crocodile-regional-ruckus) as a package within conda env

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+if __name__ == '__main__':
+    setup(
+      name='crocodileregionalruckus',
+      packages=['crocodileregionalruckus'],
+      version='0.1',
+      package_dir={'crocodileregionalruckus': 'crocodileregionalruckus'},
+    )
